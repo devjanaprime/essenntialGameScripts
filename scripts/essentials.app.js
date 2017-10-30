d7e2d029f5be3e371db29dea8af183bfe07a3bb0
devjanaprime <dev@primeacademy.io>
2017-10-29 22:24:25 -0500

    some simple styling

--- scripts/essentials.app.js
+++ scripts/essentials.app.js
@@ -1,9 +1,9 @@
 var essentialsApp = angular.module( 'essentialsApp', [] );
 
-Array.prototype.sumProperty = function( prop ){
-    var total = 0
-    for( var i = 0; i < this.length; i++ ){
-        total += this[i][prop]
-    }
-    return total
-}
+Array.prototype.sumProperty = function( property ){
+    var total=0;
+    for( var i=0; i<this.length; i++ ){
+        total+=this[ i ][ property ];
+    } // end for
+    return total;
+} //end sumProperty
